f693cee8b8f1b2c0c9e9fa5c220be6a1d6b6ac67
makhoney <115995465+makhoney@users.noreply.github.com>
2025-08-31 02:37:21 +0700

    Update tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@your-ui-lib/**/*.{js,ts,jsx,tsx}" // если используешь shadcn/ui
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
